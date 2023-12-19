25f0d2ae0ae79d3ad09c1d7dd6f4a01bc8d4fd0d
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-12-19 18:54:04 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import AdminSingIn from "./view/adminSingIn.tsx";
+function App() {
+
+
+  return (
+    <>
+<div><AdminSingIn/></div>
+    </>
+  )
+}
+
+export default App
---
+++ src/view/adminSingIn.tsx
@@ -0,0 +1,11 @@
+
+
+function AdminSingIn(){
+
+    return<section className={'bg-gray-200 w-full h-screen'}>
+        <div className={'bg-white w-4/5 h-4/5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-3xl   drop-shadow-2xl '}>
+          <h1>Welcome To Evergreen</h1>
+        </div>
+    </section>
+}
+export default AdminSingIn;
