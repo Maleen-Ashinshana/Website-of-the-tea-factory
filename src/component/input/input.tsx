397acf67d4c0c4fc741f481df35900949a60eb8c
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-12-20 20:59:21 +0530

    update admin and owner login page v1.1

--- src/component/input/input.tsx
+++ src/component/input/input.tsx
@@ -9,7 +9,7 @@ function Input(props: Props) {
     return<div className={''}>
         <label htmlFor={props.name} className={"block"}></label>
         {/*<input type={props.type} id={props.name} placeholder={props.placeholder} className={props.className} />*/}
-        <input type={props.type} id={props.name} placeholder={props.placeholder} className={'border-2 border-black outline-none w-60 h-8 mt-[30px] flex left-0 right-0 top-0 bottom-0 m-auto'}/>
+        <input type={props.type} id={props.name} placeholder={props.placeholder} className={' bg-gray-200 rounded-[50px]  outline-none w-80 h-9 mt-[30px] flex left-0 right-0 top-0 bottom-0 m-auto  contrast-more:border-slate-400 contrast-more:placeholder-slate-500'}/>
     </div>
 
 }
